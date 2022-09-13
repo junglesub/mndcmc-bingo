5873f81f50ec1101e68bbcbf9e2824be9dd9951c
junglesub <junglesubmarine@gmail.com>
2022-09-13 15:18:30 +0900

    feat: base ui with shuffle feat

---
+++ web/src/tools/shuffle.js
@@ -0,0 +1,16 @@
+export const shuffle = (array) => {
+  var i = array.length,
+    j = 0,
+    temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    // swap randomly chosen element with current element
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+};
