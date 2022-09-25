e4e577584d5181f66f9ac060cc111b1f2a35419b
junglesub <junglesubmarine@gmail.com>
2022-09-25 14:17:49 +0900

    chore: refactor using react page components

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,38 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import { shuffle } from "./tools/shuffle";
+import MainTable from "./pages/MainTable.jsx";
 
 function App() {
-  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  const shuffleNumbers = () => {
-    console.log(shuffle(items));
-    setItems([...shuffle(items)]);
-  };
   return (
     <div className="App">
       <h1 className="title">빙고</h1>
-      <table className="mainTable">
-        <tr>
-          <td className="fill">{items[0]}</td>
-          <td>{items[1]}</td>
-          <td className="fill">{items[2]}</td>
-        </tr>
-        <tr>
-          <td>{items[3]}</td>
-          <td className="fill">{items[4]}</td>
-          <td>{items[5]}</td>
-        </tr>
-        <tr>
-          <td className="fill">{items[6]}</td>
-          <td>{items[7]}</td>
-          <td className="fill">{items[8]}</td>
-        </tr>
-      </table>
-      <div className="buttons">
-        <div className="shuffle-btn btn" onClick={() => shuffleNumbers()}>
-          섞기
-        </div>
-      </div>
+      <MainTable />
     </div>
   );
 }
